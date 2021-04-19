7710e9e935ae12d184b08301e16d852b4631a5eb
Allison Strandberg <allisonstrandberg@gmail.com>
2021-04-18 21:32:30 -0400

    Make footer text smaller

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <>
       <footer className="flex justify-center w-full p-8 font-josefin bg-beige">
         <div className="bg-white bg-opacity-40 p-4 rounded">
-          <a href="https://brickbottom.org/" className="text-2xl md:text-3xl">brickbottom.org</a>
+          <a href="https://brickbottom.org/" className="text-base md:text-xl">brickbottom.org</a>
         </div>
       </footer>
     </>
