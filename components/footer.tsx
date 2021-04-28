f9753fbba7d4058dd105b0fd204687dc3b30eca0
Allison Strandberg <allisonstrandberg@gmail.com>
2021-04-27 20:51:31 -0400

    Add teal stripe to artist pages

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,16 +1,16 @@
 export default function Footer() {
   return (
     <>
-      <footer className="flex justify-center w-full p-8 font-josefin bg-gray-700">
+      <footer className="flex justify-center w-full p-8 font-josefin bg-gray-700 border-t-32 border-teal">
         <div className="w-1/2 flex justify-around">
           <a
-            className="flex items-center text-base md:text-xl bg-gray-600 p-2 rounded-sm transition duration-300 hover:bg-gray-500"
+            className="flex items-center mx-2 text-base md:text-xl bg-gray-600 p-2 rounded-sm transition duration-300 hover:bg-gray-500"
             href="https://brickbottom.org/"
             target="_blank"
           >
             brickbottom.org
           </a>
-          <form action="https://www.paypal.com/donate" method="post" target="_top" className="bg-gray-600 rounded-sm transition duration-300 hover:bg-gray-500">
+          <form action="https://www.paypal.com/donate" method="post" target="_top" className="mx-2 bg-gray-600 rounded-sm transition duration-300 hover:bg-gray-500">
             <input type="hidden" name="hosted_button_id" value="4ZE5WWVVMRQHQ" />
             <input
               className="pt-2"
