5ab80470189aa346bb75dde175251d61505a1118
Allison Strandberg <allisonstrandberg@gmail.com>
2021-04-30 20:51:28 -0400

    Change menu font so bold is bolder

--- components/menuLink.tsx
+++ components/menuLink.tsx
@@ -13,14 +13,14 @@ export default function MenuLink({ href, text, internal, indent, className }: Me
   if (internal) {
     component = (
       <Link href={href}>
-        <a className={`py-2 hover:text-blue-200 transition duration-300 font-josefin ${indent ? "pl-2" : ""} ${className ? className : ""}`}>
+        <a className={`py-2 hover:text-blue-200 transition duration-300 font-josefin-sans ${indent ? "pl-2" : ""} ${className ? className : ""}`}>
           {text}
         </a>
       </Link>
     );
   } else {
     component = (
-      <a className={`py-2 hover:text-blue-200 transition duration-300 font-josefin ${indent ? "pl-2" : ""} ${className ? className : ""}`} href={href} target="_blank">
+      <a className={`py-2 hover:text-blue-200 transition duration-300 font-josefin-sans ${indent ? "pl-2" : ""} ${className ? className : ""}`} href={href} target="_blank">
         {text}
       </a>
     );
